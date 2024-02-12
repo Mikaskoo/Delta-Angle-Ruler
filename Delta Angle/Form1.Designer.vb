e5a7a86cc8109bb2c4e9a87aef502cc02bc9f9b9
Mika Kahilainen <Mika Kahilainen@ROHJAKE>
2024-02-12 09:59:06 +0200

    Add project files.

---
+++ Delta Angle/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TxtBoxResult1 = New TextBox()
+        Label4 = New Label()
+        TxtBoxResult2 = New TextBox()
+        BtnCopy1 = New Button()
+        BtnCopy2 = New Button()
+        LstBoxResults = New ListBox()
+        BtnDelete = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.FromArgb(CByte(159), CByte(159), CByte(159))
+        Label1.Location = New Point(18, 22)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(60, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Angle 1"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(160, 19)
+        TextBox1.Margin = New Padding(2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(63, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.FromArgb(CByte(159), CByte(159), CByte(159))
+        Label2.Location = New Point(18, 52)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(60, 20)
+        Label2.TabIndex = 0
+        Label2.Text = "Angle 2"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(160, 50)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(63, 27)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.FromArgb(CByte(159), CByte(159), CByte(159))
+        Label3.Location = New Point(18, 84)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(90, 20)
+        Label3.TabIndex = 0
+        Label3.Text = "Difference 1"
+        ' 
+        ' TxtBoxResult1
+        ' 
+        TxtBoxResult1.Location = New Point(160, 81)
+        TxtBoxResult1.Margin = New Padding(2)
+        TxtBoxResult1.Name = "TxtBoxResult1"
+        TxtBoxResult1.ReadOnly = True
+        TxtBoxResult1.Size = New Size(63, 27)
+        TxtBoxResult1.TabIndex = 1
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.FromArgb(CByte(159), CByte(159), CByte(159))
+        Label4.Location = New Point(18, 115)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(90, 20)
+        Label4.TabIndex = 0
+        Label4.Text = "Difference 2"
+        ' 
+        ' TxtBoxResult2
+        ' 
+        TxtBoxResult2.Location = New Point(160, 112)
+        TxtBoxResult2.Margin = New Padding(2)
+        TxtBoxResult2.Name = "TxtBoxResult2"
+        TxtBoxResult2.ReadOnly = True
+        TxtBoxResult2.Size = New Size(63, 27)
+        TxtBoxResult2.TabIndex = 1
+        ' 
+        ' BtnCopy1
+        ' 
+        BtnCopy1.BackgroundImageLayout = ImageLayout.None
+        BtnCopy1.Location = New Point(274, 81)
+        BtnCopy1.Margin = New Padding(2)
+        BtnCopy1.Name = "BtnCopy1"
+        BtnCopy1.Size = New Size(71, 27)
+        BtnCopy1.TabIndex = 2
+        BtnCopy1.Text = "Copy"
+        BtnCopy1.UseVisualStyleBackColor = True
+        ' 
+        ' BtnCopy2
+        ' 
+        BtnCopy2.Location = New Point(274, 112)
+        BtnCopy2.Margin = New Padding(2)
+        BtnCopy2.Name = "BtnCopy2"
+        BtnCopy2.Size = New Size(71, 27)
+        BtnCopy2.TabIndex = 2
+        BtnCopy2.Text = "Copy"
+        BtnCopy2.UseVisualStyleBackColor = True
+        ' 
+        ' LstBoxResults
+        ' 
+        LstBoxResults.FormattingEnabled = True
+        LstBoxResults.Location = New Point(160, 142)
+        LstBoxResults.Margin = New Padding(2)
+        LstBoxResults.Name = "LstBoxResults"
+        LstBoxResults.SelectionMode = SelectionMode.MultiSimple
+        LstBoxResults.Size = New Size(63, 604)
+        LstBoxResults.TabIndex = 3
+        ' 
+        ' BtnDelete
+        ' 
+        BtnDelete.Location = New Point(274, 719)
+        BtnDelete.Margin = New Padding(2)
+        BtnDelete.Name = "BtnDelete"
+        BtnDelete.Size = New Size(71, 27)
+        BtnDelete.TabIndex = 2
+        BtnDelete.Text = "Delete"
+        BtnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(120F, 120F)
+        AutoScaleMode = AutoScaleMode.Dpi
+        AutoSize = True
+        BackgroundImage = My.Resources.Resources._800x800
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(1008, 1008)
+        Controls.Add(LstBoxResults)
+        Controls.Add(BtnDelete)
+        Controls.Add(BtnCopy2)
+        Controls.Add(BtnCopy1)
+        Controls.Add(TxtBoxResult2)
+        Controls.Add(Label4)
+        Controls.Add(TxtBoxResult1)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        DoubleBuffered = True
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Margin = New Padding(2)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Delta Angle Ruler"
+        TopMost = True
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtBoxResult1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TxtBoxResult2 As TextBox
+    Friend WithEvents BtnCopy1 As Button
+    Friend WithEvents BtnCopy2 As Button
+    Friend WithEvents LstBoxResults As ListBox
+    Friend WithEvents BtnDelete As Button
+
+End Class
---
+++ Delta Angle/Form1.vb
@@ -0,0 +1,206 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim alku1 As Point
+    Dim loppu1 As Point
+    Dim alku2 As Point
+    Dim loppu2 As Point
+    Dim kulma1 As Double
+    Dim kulma2 As Double
+    Dim tulokset As ArrayList
+    Private Enum DragMode
+        Enabled
+        Disabled
+    End Enum
+    Private Siirtoalue1 As New Rectangle(400, 0, 800, 400) ' Action area 1 for mover
+    Private Siirtoalue2 As New Rectangle(400, 400, 800, 800) 'Action area 2 for mover    
+    Private IsMoving1 As DragMode = DragMode.Disabled ' Set the Drag Mode
+    Private IsMoving2 As DragMode = DragMode.Disabled ' Set the Drag Mode
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackColor = Color.Beige
+        TransparencyKey = BackColor
+        TextBox1.ForeColor = Color.Red
+        TextBox2.ForeColor = Color.Blue
+        TextBox1.BackColor = TextBox1.BackColor
+        TextBox2.BackColor = TextBox2.BackColor
+        BtnDelete.Enabled = False
+        'Dim myGraphics As Graphics = Me.CreateGraphics
+        'Dim NotificationText As String = New String("Drag here")
+        'Dim NotificationPoint1 As Point = New Point(600, 200)
+        'Dim NotificationFont As Font = New Font("Arial", 12)
+        'Dim NotificationBrush1 As Brush = New Drawing.SolidBrush(Color.Red)
+        'Dim NotificationBrush2 As Brush = New Drawing.SolidBrush(Color.Blue)
+        'myGraphics.DrawString(NotificationText, NotificationFont, NotificationBrush1, 600, 200)
+        'Dim NotificationBrush As Brush = New Drawing.SolidBrush(Color.Blue)
+        'myGraphics.DrawString(NotificationText, NotificationFont, NotificationBrush2, 600, 600)
+    End Sub
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        ' ACTIVATE DRAG MODE IF MOVER AREA IS CLICKED
+        If Siirtoalue1.Contains(e.Location) Then IsMoving1 = DragMode.Enabled
+        If Siirtoalue2.Contains(e.Location) Then IsMoving2 = DragMode.Enabled
+    End Sub
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        ' DISABLE DRAG MODE WHEN MOUSE BUTTON IS RELEASED
+        If IsMoving1 = DragMode.Enabled Then IsMoving1 = DragMode.Disabled
+        If IsMoving2 = DragMode.Enabled Then IsMoving2 = DragMode.Disabled
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BtnCopy1.Click
+        LstBoxResults.Items.Add(TxtBoxResult1.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles BtnCopy2.Click
+        LstBoxResults.Items.Add(TxtBoxResult2.Text)
+    End Sub
+    Private Sub Form2_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+
+
+
+        Dim vektori1 As Pen
+        vektori1 = New Pen(Color.Red, 3)
+        Dim spotti1 As Pen
+        spotti1 = New Pen(Color.Red, 3)
+        Dim vektori2 As Pen
+        vektori2 = New Pen(Color.Blue, 3)
+        Dim spotti2 As Pen
+        spotti2 = New Pen(Color.Blue, 3)
+        Dim keskipiste1 As Point
+        keskipiste1.X = 600
+        keskipiste1.Y = 200
+        Dim keskipiste2 As Point
+        keskipiste2.X = 600
+        keskipiste2.Y = 600
+        If IsMoving1 = DragMode.Enabled Then
+            Console.WriteLine("painetaan 1")
+            Refresh()
+            Dim eroX1 As Short
+            Dim eroY1 As Short
+
+            eroX1 = keskipiste1.X - e.Location.X
+            eroY1 = keskipiste1.Y - e.Location.Y
+            CreateGraphics.DrawLine(vektori1, e.Location.X, e.Location.Y, eroX1 + keskipiste1.X, eroY1 + keskipiste1.Y)
+
+            alku1.X = e.Location.X
+            alku1.Y = e.Location.Y
+            loppu1.X = eroX1 + keskipiste1.X
+            loppu1.Y = eroY1 + keskipiste1.Y
+            'Me.CreateGraphics.DrawRectangle(spotti1, keskipiste1.X - 2, keskipiste1.Y - 2, 4, 4)
+            Console.WriteLine(e.Location)
+            Console.WriteLine(keskipiste1)
+            Console.WriteLine("deltax" & eroX1)
+            Console.WriteLine("deltay" & eroY1)
+            eroY1 = eroY1
+            kulma1 = Math.Atan(eroY1 / eroX1)
+            kulma1 = kulma1 * 180.0 / Math.PI
+            Console.WriteLine(kulma1)
+            TextBox1.Text = Math.Round(kulma1, 1)
+            Console.WriteLine()
+            Console.WriteLine(vektori1)
+            Console.WriteLine(alku1)
+            Console.WriteLine(loppu1)
+
+        End If
+        If IsMoving2 = DragMode.Enabled Then
+            Console.WriteLine("painetaan 2")
+            Refresh()
+
+            Dim eroX2 As Short
+            Dim eroY2 As Short
+
+
+
+
+
+            alku2.X = e.Location.X
+            alku2.Y = e.Location.Y
+            eroX2 = keskipiste2.X - e.Location.X
+            eroY2 = keskipiste2.Y - e.Location.Y
+            CreateGraphics.DrawLine(vektori2, e.Location.X, e.Location.Y, eroX2 + keskipiste2.X, eroY2 + keskipiste2.Y)
+            alku2.X = e.Location.X
+            alku2.Y = e.Location.Y
+            loppu2.X = eroX2 + keskipiste2.X
+            loppu2.Y = eroY2 + keskipiste2.Y
+            ' Me.CreateGraphics.DrawRectangle(spotti2, keskipiste2.X - 2, keskipiste2.Y - 2, 4, 4)
+            Console.WriteLine(e.Location)
+            Console.WriteLine(keskipiste2)
+            Console.WriteLine("deltax" & eroX2)
+            Console.WriteLine("deltay" & eroY2)
+            eroY2 = eroY2
+            kulma2 = Math.Atan(eroY2 / eroX2)
+            kulma2 = kulma2 * 180.0 / Math.PI
+            Console.WriteLine(kulma2)
+            TextBox2.Text = Math.Round(kulma2, 1)
+            Console.WriteLine()
+            Console.WriteLine(vektori2)
+            Console.WriteLine(alku2)
+            Console.WriteLine(loppu2)
+        End If
+
+        If IsMoving1 = DragMode.Disabled And IsMoving2 = DragMode.Disabled Then
+            Console.WriteLine("ei paineta")
+            'Me.Refresh()
+            'Console.Beep()
+            Dim vektor1 As Pen
+            vektor1 = New Pen(Color.Red, 3)
+
+            Dim vektor2 As Pen
+            vektor2 = New Pen(Color.Blue, 3)
+
+            CreateGraphics.DrawLine(vektor1, alku1, loppu1)
+            CreateGraphics.DrawLine(vektor2, alku2, loppu2)
+            'Me.CreateGraphics.DrawLine(vektori1, alku1.X, alku1.Y, eroX1 + keskipiste1.X, eroY1 + keskipiste1.Y)
+            'Me.CreateGraphics.DrawLine(vektori2, alku2.X, alku2.Y, eroX2 + keskipiste2.X, eroY2 + keskipiste2.Y)
+            CreateGraphics.DrawRectangle(spotti1, keskipiste1.X - 2, keskipiste1.Y - 2, 4, 4)
+            CreateGraphics.DrawRectangle(spotti2, keskipiste2.X - 2, keskipiste2.Y - 2, 4, 4)
+            Console.WriteLine()
+            Console.WriteLine(vektori1)
+            Console.WriteLine(alku1)
+            Console.WriteLine(loppu1)
+            Console.WriteLine()
+            Console.WriteLine(vektori2)
+            Console.WriteLine(alku2)
+            Console.WriteLine(loppu2)
+            TxtBoxResult1.Text = Math.Abs(Math.Round(kulma1 - kulma2, 1))
+            TxtBoxResult2.Text = Math.Abs(Math.Round(180 - Math.Abs(kulma1 - kulma2), 1))
+
+            Dim rajaviivat As Pen
+            rajaviivat = New Pen(Color.Black, 1)
+            ''Me.CreateGraphics.DrawRectangle(rajaviivat, 500, 0, 300, 300)
+            ''Me.CreateGraphics.DrawRectangle(rajaviivat, 500, 300, 300, 300)
+
+
+
+
+
+        End If
+    End Sub
+    Private Sub ListBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles LstBoxResults.KeyDown
+        'Press Control+C and selected values are copied to clipboard
+        If e.Control AndAlso e.KeyCode = Keys.C Then
+            Dim copy_buffer As New System.Text.StringBuilder
+            For Each item As Object In LstBoxResults.SelectedItems
+                copy_buffer.AppendLine(item.ToString)
+            Next
+            If copy_buffer.Length > 0 Then
+                Clipboard.SetText(copy_buffer.ToString)
+            End If
+        End If
+    End Sub
+
+    Private Sub ResultListChanged(sender As Object, e As EventArgs) Handles LstBoxResults.SelectedIndexChanged
+        If LstBoxResults.SelectedItem = "" Then
+            BtnDelete.Enabled = False
+        Else
+            BtnDelete.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        LstBoxResults.Items.RemoveAt(LstBoxResults.SelectedIndex)
+    End Sub
+
+    Private Sub Form1_MouseEnter(sender As Object, e As EventArgs) Handles MyBase.MouseEnter
+
+    End Sub
+End Class
